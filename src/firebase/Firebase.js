c70126913a99a6343f70220b9588f1a238951451
José Pedro Nunes Pereira <Jospnunes@gmail.com>
2022-12-16 20:54:33 -0300

    Update Firebase.js

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -3,12 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCaLLMnkuPtNzmkWiDUn_FrlvP9ll2I5S0",
-  authDomain: "nursehelp-3033.firebaseapp.com",
-  projectId: "nursehelp-3033",
-  storageBucket: "nursehelp-3033.appspot.com",
-  messagingSenderId: "5529362373",
-  appId: "1:5529362373:web:045e64c433f5e9e8f3f5ac",
+ 
 };
 
 
